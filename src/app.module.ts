efbfa88ebbd235aace5d60e8b6d1832d75445b85
MarcoEsquivel <marco.esquivel@catolica.edu.sv>
2023-07-04 11:46:43 -0600

    port

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
       },
       type: 'postgres',
       host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
