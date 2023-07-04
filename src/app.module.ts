e1fe4d1fa66681b595bf2153dd95ed7741b96910
MarcoEsquivel <marco.esquivel@catolica.edu.sv>
2023-07-04 13:04:31 -0600

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,12 +14,7 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      ssl: process.env.STAGE === 'prod' ? true : false,
-      extra: {
-        ssl: process.env.STAGE === 'prod'
-          ? {rejectUnauthorized: false}
-          : null,
-      },
+      
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
